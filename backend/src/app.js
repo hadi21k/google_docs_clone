03affe9fcf80832086165483e4b204b771230d1a
hadi21k <hadidyeb@gmail.com>
2023-10-29 17:06:21 +0200

    set secure true

--- backend/src/app.js
+++ backend/src/app.js
@@ -57,7 +57,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
       sameSite: "none",
-      secure: false
+      secure: true,
       // httpOnly: true,
     },
     store: sessionStore,

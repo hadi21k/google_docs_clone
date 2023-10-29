3d43793f9c6789af723430a0741f97cdaaf58a4c
hadi21k <hadidyeb@gmail.com>
2023-10-29 16:02:45 +0200

    fix sameSite for cookie to none on production

--- backend/src/app.js
+++ backend/src/app.js
@@ -47,7 +47,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
     },
